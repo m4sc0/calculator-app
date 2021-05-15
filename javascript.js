736fbe812053211337bc413879d135058e87cbb8
Masco <phalsco@web.de>
2021-05-15 07:39:42 +0200

    Saving work from 2 hours

--- javascript.js
+++ javascript.js
@@ -1,24 +1,7 @@
-function setTheme(themeName) {
-    localStorage.setItem('theme', themeName);
-    document.documentElement.className = themeName;
-}
-
-// function to toggle between light and dark theme
 function toggleTheme() {
-    if (localStorage.getItem('theme') === 'theme-dark') {
-        setTheme('theme-light');
+    if(document.getElementById("body").classList.contains("theme-dark")) {
+        document.getElementById("body").classList.replace("theme-dark","theme-light");
     } else {
-        setTheme('theme-dark');
+        document.getElementById("body").classList.replace("theme-light","theme-dark");
     }
 }
-
-// Immediately invoked function to set the theme on initial load
-(function () {
-    if (localStorage.getItem('theme') === 'theme-dark') {
-        setTheme('theme-dark');
-        document.getElementById('slider').checked = false;
-    } else {
-        setTheme('theme-light');
-      document.getElementById('slider').checked = true;
-    }
-})();
